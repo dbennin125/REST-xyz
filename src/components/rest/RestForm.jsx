f8398a65c902a0cef7a22c42d7bc1b2c21ce85bb
Dan B <dbennington125@gmail.com>
2020-07-22 15:13:25 -0700

    radio buttons work and are tested

--- src/components/rest/RestForm.jsx
+++ src/components/rest/RestForm.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RestForm = ({ text, method }) => (
-  <>
-    <input type="text" name="text" value={text}/>
-    <input type="radio" name="method" value={method}/>
-  </>
+const RestForm = ({ URL, method }) => (
+  <form>
+    <input type="text" name="text" value={URL}/>
+    <label>
+      <input type="radio" name="method" value="GET" checked={method === 'GET'}/>
+        GET
+    </label>
+    <label>
+      <input type="radio" name="method" value="PUT" checked={method === 'PUT'}/>
+        PUT
+    </label>
+    <label>
+      <input type="radio" name="method" value="POST" checked={method === 'POST'}/>
+        POST
+    </label>
+    <label>
+      <input type="radio" name="method" value="DELETE" checked={method === 'DELETE'}/>
+        DELETE
+    </label>
+  </form>
 );
 
 RestForm.propTypes = {
-  text: PropTypes.string.isRequired,
+  URL: PropTypes.string.isRequired,
   method: PropTypes.string.isRequired
 };
 
--- src/components/rest/RestForm.test.jsx
+++ src/components/rest/RestForm.test.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import RestForm from './RestForm';
 
+
 describe('RestForm component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<RestForm 
+      URL="Hello World!" 
+      method="GET"/>);
+  });
   it('has a text input with a value set to text prop', () => {
-    const wrapper = shallow(<RestForm text="Hello World!"  method="GET"/>);
     expect(wrapper.find('input[name="text"]').prop('value')).toEqual('Hello World!');
   });
-  it('has a radio input with a value set to text prop', () => {
-    const wrapper = shallow(<RestForm text="Hello World!" method="GET" />);
-    expect(wrapper.find('input[name="method"]').prop('value')).toEqual('GET');
+  it('has a GET radio input with a value set to method prop', () => {
+    expect(wrapper.find('input[name="method"]').findWhere(n => n.prop('checked')).prop('value')).toEqual('GET');
   });
+
+ 
 });
   
